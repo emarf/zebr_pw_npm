2f78c9fb7f8ad7bb32736a5933f2f21e84d73583
emarf <rivashkevich@solvd.com>
2022-02-02 17:27:36 +0300

    change config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,14 +10,14 @@ const config: PlaywrightTestConfig = {
     screenshot: 'only-on-failure',
   },
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
@@ -29,17 +29,17 @@ const config: PlaywrightTestConfig = {
       '@zebrunner/javascript-agent-playwright',
       {
         // enabled: true,
-        reportingServerHostname: 'https://webdriver.zebrunner.com',
+        // reportingServerHostname: 'https://webdriver.zebrunner.com',
         // reportingProjectKey: 'DEF',
         // reportingRunDisplayName: 'PW-tests conf',
-        reportingRunBuild: 'alpha-1 conf',
-        reportingRunEnvironment: 'STAGE conf',
+        // reportingRunBuild: 'alpha-1 conf',
+        // reportingRunEnvironment: 'STAGE conf',
         // reportingNotificationNotifyOnEachFailure: true,
-        reportingNotificationSlackChannels: 'channel1,channel2',
-        reportingNotificationMsTeamsChannels: 'channel1,channel2',
-        reportingNotificationEmails: 'channel1,channel2',
-        reportingMilestoneId: '1',
-        reportingMilestoneName: 'test',
+        // reportingNotificationSlackChannels: 'channel1,channel2',
+        // reportingNotificationMsTeamsChannels: 'channel1,channel2',
+        // reportingNotificationEmails: 'channel1,channel2',
+        // reportingMilestoneId: '1',
+        // reportingMilestoneName: 'test',
         // pwConcurrentTasks: 19,
       },
     ],
